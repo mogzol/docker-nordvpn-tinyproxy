ebd60c7c71f09a560c35722ff1bf95d909f68f7e
nhyatt <nhyatt@smoothnet.org>
2023-12-28 12:02:51 -0600

    corrects healthcheck logic

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -2,7 +2,7 @@
 # shellcheck disable=SC2236
 # SC2236 is a stylistic issue that does not affect correctness and is invalid for inverting a variable check "! -z ${VAR+x}"
 
-STORE_REMOTE_IP=false
+STORE_REMOTE_IP="false"
 
 do_fatal() {
   printf '[%b] %s\n' "\033[91mFATAL\033[0m" "${@}"
@@ -27,28 +27,27 @@ getPublicIP() {
   # curl lookups
   for i in "https://icanhazip.com" "https://ifconfig.me" "https://api.ipify.org" "https://bot.whatismyipaddress.com" "https://ipinfo.io/ip" "https://ipecho.net/plain"; do
     if ip=$(curl --fail --silent "${i}") && [ ! -z ${ip+x} ]; then
-      do_pass "Public IP: ${ip}"
+      printf '%s' "${ip}"
       return 0
     fi
   done
 
   # DNS Lookups
   if ip=$(dig -4 @ns1.google.com TXT o-o.myaddr.l.google.com +short | sed -e 's/"//g') && [ ! -z ${ip+x} ]; then
-    do_pass "Public IP: ${ip}"
+    printf '%s' "${ip}"
     return 0
   fi
 
   if ip=$(dig -4 @resolver1.opendns.com A myip.opendns.com +short | sed -e 's/"//g') && [ ! -z ${ip+x} ]; then
-    do_pass "Public IP: ${ip}"
+    printf '%s' "${ip}"
     return 0
   fi
 
   if ip=$(dig -4 @1.0.0.1 txt ch whoami.cloudflare +short | sed -e 's/"//g') && [ ! -z ${ip+x} ]; then
-    do_pass "Public IP: ${ip}"
+    printf '%s' "${ip}"
     return 0
   fi
 
-  do_error "unable to determine public IP address"
   return 100
 }
 
@@ -58,29 +57,36 @@ while [ ${#} -gt 0 ]; do
       set -x
       shift;;
     -s|--store)
-      STORE_REMOTE_IP=true
+      STORE_REMOTE_IP="true"
       shift;;
   esac
 done
 
 # get current external IP
-if DIP=$(getPublicIP) && [ ! -z ${DIP+x} ]; then
+if DIP=$(getPublicIP) && [ -z ${DIP+x} ]; then
   do_fatal "Unable to detect real-world IP."
   exit 100
 fi
 
-if [ ${STORE_REMOTE_IP} ]; then
+if [ ${STORE_REMOTE_IP} = "true" ]; then
   if ! printf 'REMOTE_IP="%s"\n' "${DIP}" > /tmp/preserved-ip; then
     do_fatal "Unable to store ip to file: /tmp/preserved-ip"
   fi
+  do_pass "Current public ip: ${DIP}"
 else
+  if [ ! -e "/tmp/preserved-ip" ]; then
+    do_fatal "Unable to read original IP for comparison"
+  fi
   if ! . /tmp/preserved-ip; then
     do_fatal "Unable to read stored ip from file: /tmp/preserved-ip"
   fi
   if [ -z ${REMOTE_IP+x} ]; then
     do_fatal "Unable to read stored ip from file: /tmp/preserved-ip"
   fi
+  do_info "Original ip: ${REMOTE_IP}"
+  do_info "Current ip:  ${DIP}"
   if [ "$(printf '%s' "${REMOTE_IP}" | sed -r -e 's/^(\d+).*$/\1/p')" = "$(printf '%s' "${DIP}" | sed -r -e 's/^(\d+).*$/\1/p')" ]; then
     do_fatal "VPN has failed and might be exposing your real ip"
   fi
+  do_pass "VPN appears to be working"
 fi
